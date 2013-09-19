4e7d1f8d337637426eefc59408e856b66bc7f0a3
brennanpayne <brennan.s.payne@gmail.com>
2013-09-18 21:10:55 -0700

    Added new controller and service as dependency in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,6 @@ window.app = angular.module('ngFantasyFootball',
  
 // bundling dependencies
 window.angular.module('ngff.controllers', 
-  ['ngff.controllers.header','ngff.controllers.index','ngff.controllers.nfl']);
+  ['ngff.controllers.header','ngff.controllers.index','ngff.controllers.nfl', 'ngff.controllers.leagues']);
 window.angular.module('ngff.services', 
-  ['ngff.services.global', 'ngff.services.nfl']);
+  ['ngff.services.global', 'ngff.services.nfl', 'ngff.services.leagues']);
